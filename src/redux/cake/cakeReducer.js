a30e03efe0a2563fdc96ff69c700f0ffe564e51e
Aashiish-Pandey <ash006pandey@gmail.com>
2021-04-11 15:51:20 +0530

    reducer and store creation

---
+++ src/redux/cake/cakeReducer.js
@@ -0,0 +1,24 @@
+import {BUY_CAKE} from './cakeTypes'
+
+const initialState = {
+
+    noOfCakes:10
+}
+
+const  cakeReducer = (state = initialState, action) => {
+
+    switch(action.type) {
+
+        case BUY_CAKE: {
+
+            return {
+                ...state,
+                noOfCakes: state.noOfCakes-1
+            }
+        }
+    }
+
+
+}
+
+export default cakeReducer;
---
+++ src/redux/cake/store.js
@@ -0,0 +1,5 @@
+import {createStore} from 'redux'
+import cakeReducer from './cakeReducer'
+const store = createStore(cakeReducer)
+
+export default store;
