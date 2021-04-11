ce19d87099795b4beaf0a5c30b35fddfc2703854
Aashiish-Pandey <ash006pandey@gmail.com>
2021-04-11 13:49:45 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./App.css";
+import CakeConatiner from "./Component/CakeContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <CakeConatiner />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/CakeContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function cakeConatiner() {
+  return (
+    <div>
+      <h2>number of cakes</h2>
+      <button>Buy cake</button>
+    </div>
+  );
+}
+
+export default cakeConatiner;
